803f45f1161362d74acb3435b5ee9def3ec988ae
park kyung suk <boardguy1024@gmail.com>
2017-08-04 13:33:18 +0900

    SigninVeiw作成

---
+++ MyInsta/SigninViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SigninViewController.swift
+//  MyInsta
+//
+//  Created by park kyung suk on 2017/08/04.
+//  Copyright © 2017年 park kyung suk. All rights reserved.
+//
+
+import UIKit
+
+class SigninViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
