cfa73d36eb19dc48536f9d9be3756a6aedcb204f
park kyung suk <boardguy1024@gmail.com>
2017-08-04 15:48:11 +0900

    TabVarViewControllerを追加

---
+++ MyInsta/ProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileViewController.swift
+//  MyInsta
+//
+//  Created by park kyung suk on 2017/08/04.
+//  Copyright © 2017年 park kyung suk. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
