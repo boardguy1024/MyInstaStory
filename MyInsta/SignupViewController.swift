56797f7954848157232a91c39697aa44451529b1
park kyung suk <boardguy1024@gmail.com>
2017-08-04 14:16:15 +0900

    SignUpViewControllerを作成

---
+++ MyInsta/SignupViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignupViewController.swift
+//  MyInsta
+//
+//  Created by park kyung suk on 2017/08/04.
+//  Copyright © 2017年 park kyung suk. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signInBtnTappedForDismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
