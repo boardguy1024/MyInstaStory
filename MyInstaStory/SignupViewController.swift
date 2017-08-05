8c9b98bba7247d85f8ed30432786306d33be9592
park kyung suk <boardguy1024@gmail.com>
2017-08-05 13:57:53 +0900

    Storageをinstall storageに保存されるprofileイメージURLをDBユーザー登録時に追加する

--- MyInstaStory/SignupViewController.swift
+++ MyInstaStory/SignupViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import FirebaseAuth
 import FirebaseDatabase
+import FirebaseStorage
 
 class SignupViewController: UIViewController {
     
@@ -22,6 +23,8 @@ class SignupViewController: UIViewController {
         
         profileImageView.layer.cornerRadius = profileImageView.frame.width / 2
         profileImageView.layer.masksToBounds = true
+        profileImageView.isUserInteractionEnabled = true
+        profileImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleProfileTap)))
         
         //Text Field Extension (カスタムBlackStyleに変更)
         usernameTextField.setCustomBlackStyleTextField()
@@ -44,14 +47,61 @@ class SignupViewController: UIViewController {
                 return
             }
             
-            if let uid = user?.uid {
-                let ref = FIRDatabase.database().reference()
-                let userRef = ref.child("users")
-                let newUserRef = userRef.child(uid)
-                newUserRef.setValue(["username": self.usernameTextField.text!,
-                                     "email": self.emailTextField.text!])
+            guard let uid = user?.uid else { return }
+            //ストレージRef
+            let storageRef = FIRStorage.storage().reference().child("profile_images").child(uid)
+            if let profileImage = self.profileImageView.image, let imageData = UIImageJPEGRepresentation(profileImage, 0.1) {
+                
+                //ストレージにprofileImageを保存
+                storageRef.put(imageData, metadata: nil, completion: { (metaData, error) in
+                    
+                    if error != nil {
+                        print("Some Error to put strorage a profileImage :\(error!)")
+                        return
+                    }
+                    
+                    let profileImageUrl = metaData?.downloadURL()?.absoluteString
+                    
+                    //データベースRef
+                    let ref = FIRDatabase.database().reference()
+                    let userRef = ref.child("users")
+                    let newUserRef = userRef.child(uid)
+                    newUserRef.setValue(["username": self.usernameTextField.text!,
+                                         "email": self.emailTextField.text!,
+                                         "profileImageUrl": profileImageUrl!])
+                })
             }
         })
     }
-    
 }
+
+extension SignupViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK:- Gesture Methods
+    func handleProfileTap() {
+        //Photo Librayを表示する
+        let imagePickerCotroller = UIImagePickerController()
+        imagePickerCotroller.delegate = self
+        imagePickerCotroller.allowsEditing = true
+        present(imagePickerCotroller, animated: true, completion: nil)
+    }
+    
+    //MARK:- UIImagePicker Delegate Mathods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let seletedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            profileImageView.image = seletedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
