a13547e0847c5b15f71281103b95fbacb905e12f
park kyung suk <boardguy1024@gmail.com>
2017-09-04 17:56:24 +0900

    SettingVCにCurrentUser情報を表示

--- MyInstaStory/SettingTableViewController.swift
+++ MyInstaStory/SettingTableViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class SettingTableViewController: UITableViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = "Edit Profile"
+        
+        fetchCurrentUser()
     }
+    func fetchCurrentUser() {
     
+        Api.User.observeCurrentUser { (user) in
+        
+            guard let imageUrlString = user.profileImageUrl,
+                  let imageUrl = URL(string: imageUrlString),
+                  let userName = user.username,
+                let email = user.email else { return }
+            
+            self.profileImageView.sd_setImage(with: imageUrl)
+            self.nameTextField.text = userName
+            self.emailTextField.text = email
+        }
+    }
 }
